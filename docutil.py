cb2945de9d17ca3efbf7da0b4f6f409e264c697a
Dave Jones <dave@waveform.org.uk>
2006-03-23 05:54:46 +0000

    Fixed bug in formatSize (wasn't correctly dealing with certain with fractional base-2 logarithms) Added some more utility routines to make SQL generation easier for the new createSql and dropSql properties

--- docutil.py
+++ docutil.py
@@ -7,7 +7,8 @@ import time
 import datetime
 import math
 
-__all__ = ['makeDateTime', 'makeBoolean']
+__all__ = ['makeDateTime', 'makeBoolean', 'quoteStr', 'formatSize', 
+	'formatIdentifier']
 
 def makeDateTime(value):
 	"""Converts a date-time value from a database query to a datetime object"""
@@ -30,19 +31,35 @@ def makeBoolean(value, trueValue='Y', falseValue='N', noneValue=' ', unknownErro
 		else:
 			return unknownResult
 
+def quoteStr(s, qchar="'"):
+	return "%s%s%s" % (qchar, s.replace(qchar, qchar*2), qchar)
+
 def formatSize(value, precisePowers=True):
 	"""Formats sizes with standard K/M/G/T/etc. suffixes"""
 	power = math.log(value, 2)
-	if not precisePowers or (power == int(power)):
-		suffix = ['', 'K', 'M', 'G', 'T', 'E', 'P'][int(power / 10)]
-		size = value / (2 ** ((int(power) / 10) * 10))
+	index = int(power / 10)
+	if not precisePowers or (value % (1024 ** index) == 0):
+		suffix = ['', 'K', 'M', 'G', 'T', 'E', 'P'][index]
+		size = value / (1024 ** index)
 		if precisePowers:
 			return "%d%s" % (int(size), suffix)
 		else:
 			return "%f%s" % (size, suffix)
 	else:
 		return str(value)
-	
+
+def formatIdentifier(value):
+	"""Formats an identifier (an object name) for use in SQL, quoting as necessary"""
+	identchars = set("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_$#@0123456789")
+	firstchars = identchars - set("0123456789")
+	if len(value) == 0:
+		raise ValueError("Invalid identifier")
+	if not value[0] in firstchars:
+		return quoteStr(value, '"')
+	for c in value[1:]:
+		if not c in identchars:
+			return quoteStr(value, '"')
+	return value.upper()
 
 def main():
 	pass
