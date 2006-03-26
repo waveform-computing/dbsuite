dea94e955caba6fbe91cb14117fd79b31ca89532
Dave Jones <dave@waveform.org.uk>
2006-03-26 22:57:28 +0000

    Added find() method to the DocDatabase class to enable searching the hierarchy for a qualified name (was originally findObject in the DocOutput class of the w3 module, moved here and rewritten)

--- docdatabase.py
+++ docdatabase.py
@@ -24,6 +24,46 @@ class DocDatabase(DocObjectBase):
 		for row in cache.schemas.itervalues():
 			self.__schemas[row['name']] = DocSchema(self, cache, **row)
 
+	def find(self, qualifiedName):
+		"""Find an object in the hierarchy by its qualified name.
+		
+		Because there are several namespaces in DB2, the results of such a
+		search can only be unambiguous if an order of precedence for object
+		types is established. The order of precedence used by this method is
+		as follows:
+		
+		Schemas
+		Tablespaces
+			Tables/Views (one namespace)
+				Fields
+				Constraints
+			Indexes
+			Functions/Methods/Procedures (one namespace)
+		
+		Hence, if a schema shares a name with a tablespace, the schema will
+		be returned in preference to the tablespace. Likewise, if an index
+		shares a name with a table, the table will be returned in preference
+		to the index.
+		"""
+		parts = qualifiedName.split(".")
+		if len(parts) == 1:
+			return self.schemas.get(parts[0],
+				self.tablespaces.get(parts[0],
+				None))
+		elif len(parts) == 2:
+			schema = self.schemas[parts[0]]
+			return schema.relations.get(parts[1],
+				schema.indexes.get(parts[1],
+				schema.routines.get(parts[1],
+				None)))
+		elif len(parts) == 3:
+			relation = self.schemas[parts[0]].relations[parts[1]]
+			return relation.fields.get(parts[2],
+				relation.constraints.get(parts[2],
+				None))
+		else:
+			return None
+
 	def getTypeName(self):
 		return "Database"
 	
