fefb395841f67f8c9de84614f3f96d14cf64456b
Dave Jones <dave@waveform.org.uk>
2006-03-28 03:23:08 +0000

    Implemented the new getSystem() property getter method for schemas with specific names Added list properties for views, tables, functions, etc. sorted by name (for use in the creation of menus in the w3 output module)

--- docschema.py
+++ docschema.py
@@ -25,31 +25,52 @@ class DocSchema(DocObjectBase):
 		self.__created = row['created']
 		self.__description = row['description']
 		self.__datatypes = {}
+		self.__datatypeList = []
 		self.__relations = {}
+		self.__relationList = []
 		self.__tables = {}
+		self.__tableList = []
 		self.__views = {}
+		self.__viewList = []
 		self.__aliases = {}
+		self.__aliasList = []
 		self.__indexes = {}
+		self.__indexList = []
 		self.__routines = {}
+		self.__routineList = []
 		self.__functions = {}
+		self.__functionList = []
 		self.__methods = {}
+		self.__methodList = []
 		self.__procedures = {}
+		self.__procedureList = []
 		self.__specificRoutines = {}
 		self.__specificFunctions = {}
 		self.__specificMethods = {}
 		self.__specificProcedures = {}
 		for datatype in [cache.datatypes[(schema, name)] for (schema, name) in cache.datatypes if schema == self.name]:
 			self.__datatypes[datatype['name']] = DocDatatype(self, cache, **datatype)
+		self.__datatypeList = [x for x in self.__datatypes.itervalues()]
+		self.__datatypeList.sort(key=lambda datatype:datatype.name)
 		for tableRec in [cache.tables[(schema, name)] for (schema, name) in cache.tables if schema == self.name]:
 			table = DocTable(self, cache, **tableRec)
 			self.__tables[tableRec['name']] = table
 			self.__relations[tableRec['name']] = table
+		self.__tableList = [x for x in self.__tables.itervalues()]
+		self.__tableList.sort(key=lambda table:table.name)
 		for viewRec in [cache.views[(schema, name)] for (schema, name) in cache.views if schema == self.name]:
 			view = DocView(self, cache, **viewRec)
 			self.__views[viewRec['name']] = view
 			self.__relations[viewRec['name']] = view
+		self.__viewList = [x for x in self.__views.itervalues()]
+		self.__viewList.sort(key=lambda view:view.name)
+		# XXX Add support for aliases
+		self.__relationList = [x for x in self.__relations.itervalues()]
+		self.__relationList.sort(key=lambda relation:relation.name)
 		for indexRec in [cache.indexes[(schema, name)] for (schema, name) in cache.indexes if schema == self.name]:
 			self.__indexes[indexRec['name']] = DocIndex(self, cache, **indexRec)
+		self.__indexList = [x for x in self.__indexes.itervalues()]
+		self.__indexList.sort(key=lambda index:index.name)
 		for funcRec in [cache.functions[(schema, name)] for (schema, name) in cache.functions if schema == self.name]:
 			func = DocFunction(self, cache, **funcRec)
 			if not funcRec['name'] in self.__routines:
@@ -60,9 +81,12 @@ class DocSchema(DocObjectBase):
 			self.__functions[funcRec['name']].append(func)
 			self.__specificRoutines[funcRec['specificName']] = func
 			self.__specificFunctions[funcRec['specificName']] = func
-		# XXX Add support for aliases
+		self.__functionList = [x for x in self.__functions.itervalues()]
+		self.__functionList.sort(key=lambda function:function[0].name)
 		# XXX Add support for methods
 		# XXX Add support for stored procedures
+		self.__routineList = [x for x in self.__routines.itervalues()]
+		self.__routineList.sort(key=lambda routine:routine[0].name)
 		# XXX Add support for sequences
 		# XXX Add support for triggers
 
@@ -81,38 +105,80 @@ class DocSchema(DocObjectBase):
 		else:
 			return super(DocSchema, self).getDescription()
 
+	def getSystem(self):
+		return self.name in [
+			"NULLID",
+			"SQLJ",
+			"SYSCAT",
+			"SYSFUN",
+			"SYSIBM",
+			"SYSPROC",
+			"SYSSTAT",
+			"SYSTOOLS"
+		]
+
 	def getDatabase(self):
 		return self.parent
 
 	def __getDatatypes(self):
 		return self.__datatypes
+	
+	def __getDatatypeList(self):
+		return self.__datatypeList
 
 	def __getRelations(self):
 		return self.__relations
 	
+	def __getRelationList(self):
+		return self.__relationList
+	
 	def __getTables(self):
 		return self.__tables
+	
+	def __getTableList(self):
+		return self.__tableList
 	
 	def __getViews(self):
 		return self.__views
 	
+	def __getViewList(self):
+		return self.__viewList
+	
 	def __getAliases(self):
 		return self.__aliases
+	
+	def __getAliasList(self):
+		return self.__aliasList
 	
 	def __getIndexes(self):
 		return self.__indexes
 	
+	def __getIndexList(self):
+		return self.__indexList
+	
 	def __getRoutines(self):
 		return self.__routines
+	
+	def __getRoutineList(self):
+		return self.__routineList
 	
 	def __getFunctions(self):
 		return self.__functions
 	
+	def __getFunctionList(self):
+		return self.__functionList
+	
 	def __getMethods(self):
 		return self.__methods
 	
+	def __getMethodList(self):
+		return self.__methodList
+	
 	def __getProcedures(self):
 		return self.__procedures
+		
+	def __getProcedureList(self):
+		return self.__procedureList
 	
 	def __getSpecificRoutines(self):
 		return self.__specificRoutines
@@ -142,15 +208,25 @@ class DocSchema(DocObjectBase):
 		return self.__created
 
 	datatypes = property(__getDatatypes, doc="""The datatypes contained in the schema""")
+	datatypeList = property(__getDatatypeList, doc="""The datatypes contained in the schema, sorted by name""")
 	relations = property(__getRelations, doc="""The relations (tables, views, etc.) contained in the schema""")
+	relationList = property(__getRelationList, doc="""The relations contained in the schema, sorted by name""")
 	tables = property(__getTables, doc="""The tables contained in the schema""")
+	tableList = property(__getTableList, doc="""The tables contained in the schema, sorted by name""")
 	views = property(__getViews, doc="""The views contained in the schema""")
+	viewList = property(__getViewList, doc="""The views contained in the schema, sorted by name""")
 	aliases = property(__getAliases, doc="""The aliases contained in the schema""")
+	aliasList = property(__getAliasList, doc="""The aliases contained in the schema, sorted by name""")
 	indexes = property(__getIndexes, doc="""The indexes contained in the schema""")
+	indexList = property(__getIndexList, doc="""The indexes contained in the schema, sorted by name""")
 	routines = property(__getRoutines, doc="""The routines (methods, functions, etc.) contained in the schema""")
+	routineList = property(__getRoutineList, doc="""The routines (methods, functions, etc.) contained in the schema, sorted by name""")
 	functions = property(__getFunctions, doc="""The functions contained in the schema""")
+	functionList = property(__getFunctionList, doc="""The functions contained in the schema, sorted by name""")
 	methods = property(__getMethods, doc="""The methods contained in the schema""")
+	methodList = property(__getMethodList, doc="""The methods contained in the schema, sorted by name""")
 	procedures = property(__getProcedures, doc="""The procedures contained in the schema""")
+	procedureList = property(__getProcedureList, doc="""The procedures contained in the schema, sorted by name""")
 	specificRoutines = property(__getSpecificRoutines, doc="""The routines contained in the schema indexed by specific name""")
 	specificFunctions = property(__getSpecificFunctions, doc="""The functions contained in the schema indexed by specific name""")
 	specificMethods = property(__getSpecificMethods, doc="""The methods contained in the schema indexed by specific name""")
