e23ad71a84555bb15aff709a56e76ce81a8e14c8
Dave Jones <dave@waveform.org.uk>
2006-03-29 00:03:29 +0000

    Added ability to include line breaks in comments Started working on the function documentation generator

--- w3/__init__.py
+++ w3/__init__.py
@@ -68,8 +68,8 @@ class DocOutput(object):
 						self.writeConstraint(constraint)
 			for index in schema.indexes.itervalues():
 				self.writeIndex(index)
-		#	for routine in schema.routines.itervalues():
-		#		self.writeRoutine(routine)
+			for function in schema.functions.itervalues():
+				self.writeFunction(function)
 		for tablespace in database.tablespaces.itervalues():
 			self.writeTablespace(tablespace)
 
@@ -99,6 +99,7 @@ class DocOutput(object):
 		require converting to character entities are converted using the
 		escape() function of the xml.sax.saxutils unit.
 		"""
+		# Replace refs and fmt modifiers with HTML
 		start = 0
 		result = ''
 		while True:
@@ -123,7 +124,9 @@ class DocOutput(object):
 					result += makeTag('u', {}, matchfmt.group(2))
 			else:
 				result += text[start:]
-				return result
+				break
+		# Replace line breaks with line break tags
+		return result.replace('\n', '<br />')
 
 	def createMenu(self, item, active=True):
 		result = []
@@ -160,7 +163,6 @@ class DocOutput(object):
 		doc.authoremail = ''
 		doc.title = "%s %s" % (object.typeName, object.qualifiedName)
 		doc.sitetitle = "%s Documentation" % (self._database.name)
-		doc.description = self.formatDescription(object.description)
 		doc.keywords = [self._database.name, object.typeName, object.name, object.qualifiedName]
 		o = object
 		while not o is None:
@@ -951,6 +953,75 @@ class DocOutput(object):
 			self.writeForeignKey(constraint)
 		elif isinstance(constraint, DocCheck):
 			self.writeCheck(constraint)
+	
+	def writeFunction(self, functions):
+		# Note that this method (unlike other methods) is passed a *list* of
+		# functions (because functions can be overloaded)
+		for function in functions:
+			logging.debug("Writing documentation for function %s to %s" % (function.name, filename(function)))
+			doc = self.newDocument(function)
+			doc.addSection(id='attributes', title='Attributes')
+			doc.addPara("""The following table notes the various attributes and
+				properties of the function.""")
+			doc.addContent(makeTable(
+				head=[(
+					"Attribute",
+					"Value",
+					"Attribute",
+					"Value"
+				)],
+				data=[
+					(
+						popupLink("created.html", "Created"),
+						function.created,
+						popupLink("funcorigin.html", "Origin"),
+						function.origin,
+					),
+					(
+						popupLink("createdby.html", "Created By"),
+						escape(function.definer),
+						popupLink("funclanguage.html", "Language"),
+						function.language,
+					),
+					(
+						popupLink("functype.html", "Type"),
+						function.type,
+						popupLink("sqlaccess.html", "SQL Access"),
+						function.sqlAccess,
+					),
+					(
+						popupLink("castfunc.html", "Cast Function"),
+						function.castFunction,
+						popupLink("assignfunc.html", "Assign Function"),
+						function.assignFunction,
+					),
+					(
+						popupLink("externalaction.html", "External Action"),
+						function.externalAction,
+						popupLink("deterministic.html", "Deterministic"),
+						function.deterministic,
+					),
+					(
+						popupLink("nullcall.html", "Call on NULL"),
+						function.nullCall,
+						popupLink("fenced.html", "Fenced"),
+						function.fenced,
+					),
+					(
+						popupLink("parallelcall.html", "Parallel"),
+						function.parallel,
+						popupLink("threadsafe.html", "Thread Safe"),
+						function.threadSafe,
+					),
+				]))
+			##doc.addSection('sql', 'SQL Definition')
+			##doc.addPara("""The SQL which can be used to create the function is given
+			##	below. Note that this is not necessarily the same as the actual
+			##	statement used to create the function (it has been reconstructed from
+			##	the content of the system catalog tables and may differ in a number
+			##	of areas).""")
+			##doc.addContent(makeTag('pre', {'class': 'sql'}, self.formatSql(function.createSql)))
+			doc.write(os.path.join(self._path, filename(function)))
 
 def main():
 	pass
