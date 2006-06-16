a2b2e3eabe8a4fe510ae116573a1e6232f2c27c6
Dave Jones <dave@waveform.org.uk>
2006-06-16 20:42:48 +0000

    Fixed bug in makeDateTime (needs a list instead of a generator for unwrapping an argument list) Updated to use isinstance() instead of a type comparison (in line with Python 2.4 changes, like the creation of the basestring class)

--- input/util.py
+++ input/util.py
@@ -9,9 +9,9 @@ def makeDateTime(value):
 	"""Converts a date-time value from a database query to a datetime object"""
 	if (value is None) or (value == ""):
 		return None
-	elif type(value) == type(""):
-		return datetime.datetime(*(int(x) for x in re.match(r"(\d{4})-(\d{2})-(\d{2})-(\d{2}).(\d{2}).(\d{2}).(\d{6})\d*", value).groups()))
-	elif hasattr(value, 'value') and (type(value.value) == int):
+	elif isinstance(value, basestring):
+		return datetime.datetime(*([int(x) for x in re.match(r"(\d{4})-(\d{2})-(\d{2})-(\d{2}).(\d{2}).(\d{2}).(\d{6})\d*", value).groups()]))
+	elif hasattr(value, 'value') and isinstance(value.value, int):
 		return datetime.datetime.fromtimestamp(value.value)
 	else:
 		raise ValueError("Unable to convert date-time value")
