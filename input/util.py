4a5a55f9a606f087d393159f23dc2d726dfccb1d
Dave Jones <dave@waveform.org.uk>
2006-06-21 00:51:57 +0000

    Refined the regular expression for extracting fields from dates

--- input/util.py
+++ input/util.py
@@ -7,14 +7,15 @@ import datetime
 
 def makeDateTime(value):
 	"""Converts a date-time value from a database query to a datetime object"""
+	print value
 	if (value is None) or (value == ""):
 		return None
 	elif isinstance(value, basestring):
-		return datetime.datetime(*([int(x) for x in re.match(r"(\d{4})-(\d{2})-(\d{2})-(\d{2}).(\d{2}).(\d{2}).(\d{6})\d*", value).groups()]))
+		return datetime.datetime(*([int(x) for x in re.match(r"(\d{4})-(\d{2})-(\d{2})[T -](\d{2})[:.](\d{2})[:.](\d{2})\.(\d{6})\d*", value).groups()]))
 	elif hasattr(value, 'value') and isinstance(value.value, int):
 		return datetime.datetime.fromtimestamp(value.value)
 	else:
-		raise ValueError("Unable to convert date-time value")
+		raise ValueError('Unable to convert date-time value "%s"' % str(value))
 
 def makeBoolean(value, trueValue='Y', falseValue='N', noneValue=' ', unknownError=False, unknownResult=None):
 	"""Converts a character-based value into a boolean value"""
