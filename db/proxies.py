614de3e9632ad81ea985a87fa9425a0c8cce8c63
Dave Jones <dave@waveform.org.uk>
2006-06-21 12:58:42 +0000

    Tidied up old type() references Added TriggersDict and TriggersList classes

--- db/proxies.py
+++ db/proxies.py
@@ -9,7 +9,8 @@ class RelationsDict(object, DictMixin):
 	
 	def __init__(self, database, relations):
 		"""Initializes the dict from a list of (schemaName, relationName) tuples"""
-		if relations is None: relations = []
+		if relations is None:
+			relations = []
 		self.__database = database
 		self.__keys = relations
 		
@@ -23,7 +24,7 @@ class RelationsDict(object, DictMixin):
 		return len(self.__keys)
 	
 	def __getitem__(self, key):
-		assert type(key) == type(())
+		assert isinstance(key, tuple)
 		(schemaName, relationName) = key
 		return self.__database.schemas[schemaName].relations[relationName]
 	
@@ -39,7 +40,8 @@ class RelationsList(object):
 	
 	def __init__(self, database, relations):
 		"""Initializes the list from a list of (schemaName, relationName) tuples"""
-		if relations is None: relations = []
+		if relations is None:
+			relations = []
 		self.__database = database
 		self.__items = relations
 		
@@ -47,7 +49,7 @@ class RelationsList(object):
 		return len(self.__items)
 	
 	def __getitem__(self, key):
-		assert type(key) == int
+		assert isinstance(key, int)
 		(schemaName, relationName) = self.__items[key]
 		return self.__database.schemas[schemaName].relations[relationName]
 	
@@ -66,7 +68,8 @@ class IndexesDict(object, DictMixin):
 	
 	def __init__(self, database, indexes):
 		"""Initializes the dict from a list of (schemaName, indexName) tuples"""
-		if indexes is None: indexes = []
+		if indexes is None:
+			indexes = []
 		self.__database = database
 		self.__keys = indexes
 
@@ -80,7 +83,7 @@ class IndexesDict(object, DictMixin):
 		return len(self.__keys)
 	
 	def __getitem__(self, key):
-		assert type(key) == type(())
+		assert isinstance(key, tuple)
 		(schemaName, indexName) = key
 		return self.__database.schemas[schemaName].indexes[indexName]
 	
@@ -96,7 +99,8 @@ class IndexesList(object):
 	
 	def __init__(self, database, indexes):
 		"""Initializes the list from a list of (schemaName, indexName) tuples"""
-		if indexes is None: indexes = []
+		if indexes is None:
+			indexes = []
 		self.__database = database
 		self.__items = indexes
 	
@@ -104,13 +108,72 @@ class IndexesList(object):
 		return len(self.__items)
 	
 	def __getitem__(self, key):
-		assert type(key) == int
+		assert isinstance(key, int)
 		(schemaName, indexName) = self.__items[key]
 		return self.__database.schemas[schemaName].indexes[indexName]
 	
 	def __iter__(self):
 		for (schemaName, indexName) in self.__items:
 			yield self.__database.schemas[schemaName].indexes[indexName]
+			
+	def __contains__(self, key):
+		for i in self:
+			if i == key:
+				return True
+		return False
+
+class TriggersDict(object, DictMixin):
+	"""Presents a dictionary of triggers"""
+	
+	def __init__(self, database, triggers):
+		"""Initializes the dict from a list of (schemaName, triggerName) tuples"""
+		if triggers is None:
+			triggers = []
+		self.__database = database
+		self.__keys = triggers
+
+	def keys(self):
+		return self.__keys
+	
+	def has_key(self, key):
+		return key in self.__keys
+	
+	def __len__(self):
+		return len(self.__keys)
+	
+	def __getitem__(self, key):
+		assert isinstance(key, tuple)
+		(schemaName, triggerName) = key
+		return self.__database.schemas[schemaName].triggers[triggerName]
+	
+	def __iter__(self):
+		for k in self.__keys:
+			yield k
+			
+	def __contains__(self, key):
+		return key in self.__keys
+
+class TriggersList(object):
+	"""Presents a list of triggers"""
+	
+	def __init__(self, database, triggers):
+		"""Initializes the list from a list of (schemaName, triggerName) tuples"""
+		if triggers is None:
+			triggers = []
+		self.__database = database
+		self.__items = triggers
+	
+	def __len__(self):
+		return len(self.__items)
+	
+	def __getitem__(self, key):
+		assert isinstance(key, int)
+		(schemaName, triggerName) = self.__items[key]
+		return self.__database.schemas[schemaName].triggers[triggerName]
+	
+	def __iter__(self):
+		for (schemaName, triggerName) in self.__items:
+			yield self.__database.schemas[schemaName].triggers[triggerName]
 			
 	def __contains__(self, key):
 		for i in self:
