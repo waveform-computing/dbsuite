1b38e24041be9eab2562283979ec193f0d70e6b4
Dave Jones <dave@waveform.org.uk>
2006-10-08 12:51:15 +0000

    Fixed bug in format_size (can't calculate log of 0)

--- db/util.py
+++ db/util.py
@@ -8,17 +8,20 @@ def quote_str(s, qchar="'"):
 
 def format_size(value, precise_powers=True):
 	"""Formats sizes with standard K/M/G/T/etc. suffixes"""
-	power = math.log(value, 2)
-	index = int(power / 10)
-	if not precise_powers or (value % (1024 ** index) == 0):
-		suffix = ['', 'K', 'M', 'G', 'T', 'E', 'P'][index]
-		size = value / (1024 ** index)
-		if precise_powers:
-			return "%d%s" % (int(size), suffix)
-		else:
-			return "%f%s" % (size, suffix)
-	else:
+	if value == 0:
 		return str(value)
+	else:
+		power = math.log(value, 2)
+		index = int(power / 10)
+		if not precise_powers or (value % (1024 ** index) == 0):
+			suffix = ['', 'K', 'M', 'G', 'T', 'E', 'P'][index]
+			size = value / (1024 ** index)
+			if precise_powers:
+				return "%d%s" % (int(size), suffix)
+			else:
+				return "%f%s" % (size, suffix)
+		else:
+			return str(value)
 
 def format_ident(value, ident_quotes='"'):
 	"""Formats an identifier (an object name) for use in SQL, quoting as necessary"""
